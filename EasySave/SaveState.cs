376c6ae5e50f9c60b6a3e13cac63d5234fb234ba
Julien MAUROUX <julien.mauroux@viacesi.fr>
2022-01-28 10:03:23 +0000

    Merged PR 12: add getter and setter
    add getter and setter

--- EasySave/SaveState.cs
+++ EasySave/SaveState.cs
@@ -3,5 +3,64 @@ namespace EasySave
 {
     internal class SaveState
     {
+        SaveWork SaveWork;
+        private string State;
+        private int TotalFilesToCopy;
+        private int TotalFilesSize;
+        private int NbFileLeftToDo;
+        private int Progression;
+        FileFormat fileFormat;
+
+        public SaveState()
+        {
+
+        }
+
+        public string GetState(){
+            return State;
+           
+        }
+        public void SetState(string State){
+            this.State = State;
+        }
+        
+        public int GetTotalFilesToCopy()
+        {
+            return TotalFilesToCopy;
+        }
+        public void SetTotalFilesToCopy(int TotalFilesToCopy)
+        {
+            this.TotalFilesToCopy = TotalFilesToCopy;
+        }
+        public int GetTotalFilesSize()
+        {
+            return TotalFilesSize;
+        }
+        public void SetTotalFileSize(int TotalFilesSize)
+        {
+            this.TotalFilesSize = TotalFilesSize;
+        }
+        public int GetNbFilesLeftToDo()
+        {
+            return NbFileLeftToDo;
+        }
+        public void SetTotalFilesLeftToDo(int nbFileLeftToDo)
+        {
+            this.NbFileLeftToDo= nbFileLeftToDo;
+        }
+        
+        public int GetProgression() 
+        {
+            return Progression;
+        }
+        public void SetProgression(int Progression)
+        {
+            this.Progression=Progression;
+        }
+
+
+
     }
 }
+
+
