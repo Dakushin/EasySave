7f11c562e6b96242714da3d764097239bb454506
Rabie HAMADOUCHE <rabie.hamadouche@viacesi.fr>
2022-01-28 10:07:36 +0000

    Merged PR 14: Add log
    add log
    
    Related work items: #23

--- EasySave/Log.cs
+++ EasySave/Log.cs
@@ -6,14 +6,56 @@ namespace EasySave
     internal class Log<T> where T : FileFormat
     {
         private SaveWork saveWork;
-        private int fileSave;
+        private int fileSize;
         private T fileFormat;
-        private int fileTransferTime;
+        private float fileTransferTime;
         private string time;
 
         public Log(T fF, SaveWork sW, int fS, int fTT, string time)
         {
             
+        }
+
+        public int getFileSize()
+        {
+            return fileSize;
+        }
+
+        public void setFileSize(int FileSize)
+        {
+            this.fileSize = FileSize;
+        }
+
+        public float getFileTransferTime()
+        {
+            return fileTransferTime;
+        }
+
+        public void setFileTransferTime(float FileTransferTime)
+        {
+            this.fileTransferTime = FileTransferTime;
+        }
+
+        public string getTime()
+        {
+            return time;
+        }
+
+        public void setTime(string Time)
+        {
+            this.time = Time;
+
+        }
+
+        public T getFileFormat()
+        {
+            return fileFormat;
+        }
+
+        public void setFileFormat(T FileFormat)
+        {
+            this.fileFormat = FileFormat;
+        
         }
     }
 }
